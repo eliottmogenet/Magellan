18a5df9655c0cd32359efb64b2b84f960e9318e6
eliott mogenet <eliott.mogenet@gmail.com>
2021-09-14 23:30:44 -0700

    jquery done?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require jquery
+//= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
 
 
 
